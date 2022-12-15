d972e74fa54ce38ed3e5a83cb237b0caf6311506
hetvimakwana69 <hetvimakwana1533@gmail.com>
2022-12-15 17:50:02 +0530

    Hetvi

--- admin/control.php
+++ admin/control.php
@@ -168,14 +168,17 @@ class control extends model
 				$fetch=$run->fetch_object();
 				if(isset($_REQUEST['submit']))
 				{
-					$e_id=$_REQUEST['e_id'];
-					$e_name=$_REQUEST['e_name'];
-					$user_name=$_REQUEST['user_name'];
-					$emailid=$_REQUEST['emailid'];
-					$address=$_REQUEST['address'];
-					$contact_no=$_REQUEST['contact_no'];
+					$emp_id=$_REQUEST['emp_id'];
+					$emp_name=$_REQUEST['emp_name'];
+					$emp_add=$_REQUEST['emp_add'];
+					$username=$_REQUEST['username'];
+					$password=$_REQUEST['password'];
+					$designation=$_REQUEST['designation'];
+				    $email=$_REQUEST['email'];
+				    $contact=$_REQUEST['contact'];
+				    $driving_licence=$_REQUEST['driving_licence'];
 					
-					$arr=array("e_id"=>$e_id,"e_name"=>$e_name,"user_name"=>$user_name,"emailid"=>$emailid,"address"=>$address,"contact_no"=>$contact_no);
+					$arr=array("emp_id"=>$emp_id,"emp_name"=>$emp_name,"emp_add"=>$emp_add,"username"=>$username,"password"=>$password,"designation"=>$designation,"email"=>$email,"contact"=>$contact,"driving_licence"=>$driving_licence);
 					$res=$this->update('employee_details',$arr,$where);
 					if($res)
 					{
