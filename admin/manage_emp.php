389d676f81dc60410160c20a31bd75d1a0364abf
hetvimakwana69 <hetvimakwana1533@gmail.com>
2022-12-17 12:15:17 +0530

    hetvi

--- admin/manage_emp.php
+++ admin/manage_emp.php
@@ -55,7 +55,7 @@ include_once('header.php');
 						<td><?php echo $m->contact;?></td>
 						<td><?php echo $m->driving_licence;?></td>
 						<td><a href="edit_emp?edit_emp_id=<?php echo $m->emp_id;?>" class="btn btn-primary">Edit</a></td>
-						<td><a href="delete?del_emp_id=<?php echo $m->emp_id;?>"class="btn btn-primary">Delete</a></td>
+						<td><a href="delete?del_emp_id=<?php echo $m->emp_id;?>"class="btn btn-danger">Delete</a></td>
 					  </tr>
 					  <?php
 					    }
