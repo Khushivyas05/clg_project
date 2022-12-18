d797ed2069206706d3eddaeb6ce61a688d00a170
Khushivyas05 <khushignc@gmail.com>
2022-12-18 18:29:02 +0530

    k

--- admin/add_branch.php
+++ admin/add_branch.php
@@ -2,7 +2,7 @@
 include_once('header.php');
 ?>
 <script>
-function validate()nn
+function validate()
 {
 	var branch_name=document.forms["branchform"]["branch_name"].value;
 	if(branch_name=="" || branch_name==null)
