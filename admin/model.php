69865a043be225756175c84f6cec4154801de2a2
Khushivyas05 <khushignc@gmail.com>
2022-12-19 22:41:14 +0530

    k

--- admin/model.php
+++ admin/model.php
@@ -57,7 +57,7 @@ class model
 		$i=0;
 		foreach($where as $w)
 		{
-			echo $del.=" and $key_arr[$i]='$value_arr[$i]'";
+			$del.=" and $key_arr[$i]='$value_arr[$i]'";
 			$i++;
 		}
 		$run=$this->conn->query($del);
