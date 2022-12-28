e35a2168b2290aa198fcf99863737a9e04838ba9
Khushivyas05 <khushignc@gmail.com>
2022-12-28 18:07:18 +0530

    khushi

---
+++ web/footer.php
@@ -0,0 +1,114 @@
+<!-- grids block 5 -->
+<section class="w3l-footer-29-main">
+    <div class="footer-29">
+        <div class="container">
+            <div class="d-grid grid-col-4 footer-top-29">
+                <div class="footer-list-29 footer-1">
+                    <h6 class="footer-title-29"><a href="index.html"><span class="fa fa-truck"></span> Packers</a></h6>
+                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+                   <div class="main-social-footer-29">
+                    <a href="#facebook" class="facebook"><span class="fa fa-facebook"></span></a>
+                    <a href="#twitter" class="twitter"><span class="fa fa-twitter"></span></a>
+                    <a href="#instagram" class="instagram"><span class="fa fa-instagram"></span></a>
+                    <a href="#google-plus" class="google-plus"><span class="fa fa-google-plus"></span></a>
+                    <a href="#linkedin" class="linkedin"><span class="fa fa-linkedin"></span></a>
+                </div>
+                </div>
+                <div class="footer-list-29 footer-2">
+                </div>
+                
+                <div class="footer-list-29 footer-3">
+                   
+                  <h6 class="footer-title-29">Newsletter </h6>
+          <form action="#" class="subscribe" method="post">
+            <input type="email" name="email" placeholder="Email" required="">
+            <button><span class="fa fa-envelope-o"></span></button>
+          </form>
+          <p>Subscribe and get our weekly newsletter</p>
+          <p>We'll never share your email address</p>
+        
+              </div>
+              <div class="footer-list-29 footer-4">
+                <ul>
+                    <h6 class="footer-title-29">Quick Links</h6>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="blog.html"> Blog</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+            </div>
+            <div class="d-grid grid-col-2 bottom-copies">
+                <p class="copy-footer-29">© 2020 Packers. All rights reserved | Designed by Your Transportation Team</p>
+                 <ul class="list-btm-29">
+                        <li><a href="#link">Privacy policy</a></li>
+                        <li><a href="#link">Terms of service</a></li>
+                        <li><select name="carlist" form="carform">
+                                <option value="Language">English <span class="fa fa-angle-down"></span></option>
+                                <option value="Language">France</option>
+                                <option value="Language">Spanish </option>
+                                <option value="Language">Russian</option>
+                            </select>
+                        </li>
+                    </ul>
+            </div>
+        </div>
+    </div>
+     <!-- move top -->
+		<button onclick="topFunction()" id="movetop" title="Go to top">
+                <span class="fa fa-angle-up"></span>
+                   </button>
+                   <script>
+                       // When the user scrolls down 20px from the top of the document, show the button
+                       window.onscroll = function () {
+                           scrollFunction()
+                       };
+                
+                       function scrollFunction() {
+                           if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                               document.getElementById("movetop").style.display = "block";
+                           } else {
+                               document.getElementById("movetop").style.display = "none";
+                           }
+                       }
+                
+                       // When the user clicks on the button, scroll to the top of the document
+                       function topFunction() {
+                           document.body.scrollTop = 0;
+                           document.documentElement.scrollTop = 0;
+                       }
+                   </script>
+                   <!-- /move top -->
+</section>
+<script src="assets/js/jquery-3.3.1.min.js"></script>
+<!-- //footer-28 block -->
+</section>
+<!-- <script>
+$(function () {
+  $('.navbar-toggler').click(function () {
+    $('body').toggleClass('noscroll');
+  })
+});
+</script> -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="../../../../../../../code.jquery.com/jquery-3.4.1.slim.min.js"
+integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+</script>
+<script src="../../../../../../../cdn.jsdelivr.net/npm/popper.js%401.16.0/dist/umd/popper.min.js"
+integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+</script>
+<script src="../../../../../../../stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+</script>
+
+<!-- Template JavaScript -->
+<script src="assets/js/all.js"></script>
+<!-- Smooth scrolling -->
+<!-- <script src="assets/js/smoothscroll.js"></script> -->
+
+
+
+</body>
+</html>
+<!-- // grids block 5 -->
