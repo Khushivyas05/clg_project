b9a0fcfdd1aad84b4279a7d66690442f68c6c51c
disha_php <panchalrameshbhai81@gmail.com>
2022-12-29 17:24:41 +0530

    d

--- web/control.php
+++ web/control.php
@@ -27,10 +27,74 @@ class control extends model
             break; 
 
             case '/signup.php':
+            if(isset($_REQUEST['submit']))
+            {
+                $name=$_REQUEST['name'];
+                $address=$_REQUEST['address'];
+                $contact=$_REQUEST['contact'];
+                $email=$_REQUEST['email'];
+                $password=$_REQUEST['password'];
+                $username=$_REQUEST['username'];
+
+                $arr=array("name"=>$name,"address"=>$address,"contact"=>$contact,"email"=>$email,"password"=>$password,"username"=>$username);
+                $res=$this->insert('contact',$arr);
+
+                if($res)
+                {
+                    echo "<script>
+					alert('Inquiry Success');
+					</script>";
+                }
+                else
+                {
+                    echo "Inquiry not success";
+                }
+            }    
             include_once('signup.php');
             break;
             
             case '/login':
+            if(isset($_REQUEST['submit']))
+            {
+                $username=$_REQUEST['username'];
+				$password=$_REQUEST['password'];
+				
+                $where=array("username"=>$username,"pass"=>$pass);
+				$run=$this->select_where('customer',$where);
+
+                $res=$run->num_rows; 
+				if($res==1)
+                {
+                    
+				        $data=$run->fetch_object();
+					    $status=$data->status;
+					    if($status=="Unblock")
+                        {
+                            $_SESSION['username']=$username;
+                            echo "<script> 
+                            alert('Login Success') 
+                            window.location='index';
+                            </script>";
+                        }
+                        else
+                        {
+                            echo "<script> 
+						    alert('Login Failed due to blocked') 
+						    window.location='login';
+						    </script>";
+                        }
+
+                } 
+                else
+                {
+                    
+					echo "<script> 
+                    alert('Login Failed due wrong credentials') 
+                    window.location='index';
+                    </script>";
+                }
+				
+            }
             include_once('login.php');
             break;    
 
