f7f9c0d78d70f6d8f20fc8a7b7d2e04c4b673847
hetvimakwana69 <hetvimakwana1533@gmail.com>
2022-12-30 18:47:37 +0530

    h

--- admin/add_emp.php
+++ admin/add_emp.php
@@ -5,66 +5,81 @@ include_once('header.php');
 <script>
   function validate()
   {
-	  var username=document.forms["empform"]["username"].value;
+	var username=document.forms["empform"]["username"].value;
 	  if(username=="" || username==null)
 	  {
-		  alert("please fill out username");
-		  return false;
+		alert('please fill out the username');
+		return false;
 	  }
-	  var pass=document.forms["empform"]["password"].value;
-	   if(password=="" || password==null)
-	   {
-		   alert("please fill out the password");
-		   return false;
-	   } 
-	 var emp_name=document.forms["empform"]["emp_name"].value;
-	   if(emp_name=="" || emp_name==null)
-	   {
-		   alert("please fill out the emp_name");
-		   return false;
-	   }  
-	 var alpha=/^[A-Za-z]+$/;
+	  var password=document.forms["empform"]["password"].value;
+	  if(password=="" || password==null)
+	  {
+		alert('please fill out the password');
+		return false;
+	  }
+	  var emp_name=document.forms["empform"]["emp_name"].value;
+	  if(emp_name=="" || emp_name==null)
+	  {
+		  alert("please fill out Name");
+		  return false;
+	  }  
+	 /*var alpha=/^[A-Za-z]+$/;
 	  if(!alpha.test(emp_name))
 	  {
 		  alert("please fill only character");
 		  return false;
 	  }
-	 var emp_add=document.forms["empform"]["emp_add"].value;
-	   if(emp_add=="" || emp_add==null)
-	   {
-		   alert("please fill out the emp_add");
-		   return false;
-	   } 
-	 var designation=document.forms["empform"]["designation"].value;
-	   if(designation=="" || designation==null)
-	   {
-		   alert("please fill out the designation");
-		   return false;
-	   } 
-	 var email=document.forms["empform"]["email"].value;
+	  var emp_add=document.forms["empform"]["emp_add"].value;
+	  if(emp_add=="" || emp_add==null)
+	  {
+		  alert("please fill out emp_add");
+		  return false;
+	  } 
+	  var des = document.getElementsByName("designation");
+    if (des[0].checked == true) 
+	{
+                   
+    } 
+	else if (des[1].checked == true) 
+	{
+                  
+    } 
+	else 
+	{
+		alert('! Please Select designation');  // alert msg
+        return false;
+    } 
+	var email=document.forms["empform"]["email"].value;
 	   if(email=="" || email==null)
 	   {
-		   alert("please fill out the emailid");
+		   alert("please fill out the email");
 		   return false;
-	   } 
-	 var mail=/^[a-zA-Z0-9_]+@[a-zA-Z]+\.[a-zA-Z]{2,4}$/;
-       if(!mail.test(email))
+	   }  
+	   var mail=/^[a-zA-Z0-9_]+@[a-zA-Z]+\.[a-zA-Z]{2,4}$/;
+	if(!mail.test(email))
+	{
+		alert('Please fill proper email id');  // alert msg
+		return false;   //return false means msg show and again on same page with value not refresh page
+
+	}
+	var contact=document.forms["empform"]["contact"].value;
+	   if(contact=="" || contact==null)
 	   {
-		   alert("please proper email id");
+		   alert('please fill out the contact no');
 		   return false;
 	   }
-	 var contact_no=document.forms["empform"]["contact_no"].value;
-	   if(contact_no=="" || contact_no==null)
+      var phone=/^[0-9]{10,11}$/;
+       if(!phone.test(contact))
 	   {
-		   alert("please fill out the contact no");
+		   alert('please proper phone no');
 		   return false;
 	   }
-     var phone=/^[0-9]{10,11}$/;
-       if(!phone.test(contact_no))
+	   var driving_licence=document.forms["empform"]["driving_licence"].value;
+	   if(driving_licence=="" || driving_licence==null)
 	   {
-		   alert("please proper phone no");
+		   alert('please fill out the driving_licence no');
 		   return false;
-	   }		   
+	   }	*/		   
   }
 </script>
  	<!--banner-->	
