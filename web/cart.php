395b1c4e0d9307f18d405b6c0038fa022517007f
Khushivyas05 <khushignc@gmail.com>
2023-01-18 18:35:00 +0530

    khushi

--- web/cart.php
+++ web/cart.php
@@ -63,7 +63,7 @@ include_once('header.php');
                 </td>
                 <td class=" invert product-name"><a href="ecommerce-single.html">
                   Packaging, Shipping Packaging Box (Pack of 25 Brown)
-                  </a>n
+                  </a>
 
 
                 </td>
