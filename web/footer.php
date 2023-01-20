66bfdfa62794f7b43a8d0ee08f840443a700b1d5
hetvimakwana69 <hetvimakwana1533@gmail.com>
2023-01-20 18:11:27 +0530

    hetu

--- web/footer.php
+++ web/footer.php
@@ -56,8 +56,7 @@
                 
                        // When the user clicks on the button, scroll to the top of the document
                        function topFunction() {
-                           document.body.scrollTop = 0;
-                           document.documentElement.scrollTop = 0;
+                           $(window).scrollTop(0);
                        }
                    </script>
                    <!-- /move top -->
