6d323b95c351b174be4f9211aaa42fefae0c8154
disha_php <panchalrameshbhai81@gmail.com>
2023-02-10 17:54:08 +0530

    d

--- web/editprofile.php
+++ web/editprofile.php
@@ -1,122 +1,113 @@
 <?php
+if(isset($_SESSION['username']))
+{
+
+}
+else
+{
+    echo "<script>
+          window.location='index';
+         </script>";
+}
 include_once('header.php');
 ?>
-<script>
-function validate()
-{
-	var parcel_id=document.forms["customerform"]["cust_name"].value;
-	if(parcel_id=="" || parcel_id==null)
-	{
-		alert("Please fill out the cust_name");
-		return false;
-	}  
-	var cust_id=document.forms["customerform"]["city_name"].value;
-	if(cust_id=="" || cust_id==null)
-	{
-		alert("Please fill out the cust_id");
-		return false;
-	}
-	var gt_id=document.forms["customerform"]["cust_add"].value;
-	if(gt_id=="" || gt_id==null)
-	{
-		alert("Please fill out the cust_add");
-		return false;
-	}
-	
-	var weight=document.forms["customerform"]["contact_no"].value;
-	if(weight=="" || weight==null)
-	{
-		alert("Please fill out the contact_no");
-		return false;
-	}
-	var quantity=document.forms["customerform"]["email"].value;
-	if(quantity=="" || quantity==null)
-	{
-		alert("Please fill out the email");
-		return false;
-	}
-	var quantity=document.forms["customerform"]["username"].value;
-	if(quantity=="" || quantity==null)
-	{
-		alert("Please fill out the username");
-		return false;
-	}
-	
-}
-</script>
- 
- 	<!--banner-->	
-		   <div class="banner">
-		    	<h2>
-				<a href="manage_customer">Home</a>
-				<i class="fa fa-angle-right"></i>
-				<span>Edit profile</span>
-				</h2>
-		    </div>
-		<!--//banner-->
- 	<!--grid-->
- 	<div class="validation-system">
- 		
- 		<div class="validation-form">
- 	<!---->
-  	    
-        <form name="customerform" action="" method="post" enctype="multipart/form-data" onsubmit="return validate()">
-         	<div class="vali-form">
-            <div class="col-md-12 form-group1">
-              <label class="control-label">Customer Name</label>
-              <input type="text" placeholder="cust_name" name="cust_name" value="<?php echo $fetch->cust_name;?>" >
-            </div>
+<!-- inner banner -->
 
+<section class="w3l-inner-banner-main">
+    <div class="about-inner sear-1">
+        <div class="container">
             
-            <div class="clearfix"> </div>
+            <ul class="breadcrumbs-custom-path">
+               
+                <li><a href="index">Home <span class="fa fa-angle-double-right" aria-hidden="true"></span></a></li>
+                 <li class="active">Edit Profile</li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!-- //covers -->
+<!-- contact -->
+<section class="w3l-contacts-12" id="booking">
+    <div class="contact-top pt-5">
+        <div class="container py-md-3">
+            <div class="heading text-center mx-auto">
+                <h3 class="head">PROFILE</h3>
+               <!-- <p class="my-3 head"> Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                  Nulla mollis dapibus nunc, ut rhoncus
+                  turpis sodales quis. Integer sit amet mattis quam.</p>-->
+              </div>
+            <div class="row cont-main-top mt-5 pt-3">
+               
+                <!-- contact address -->
+               
+                <!-- //contact address -->
+                 <!-- contact form -->
+                 <div class="contacts12-main col-lg-12 pr-lg-5 pr-3">
+                   
+                    <form name="bookingform" action="" method="post" enctype="multipart/form-data" class="main-input">
+                       
+                        
+						<div class="top-inputs">
+                        Customer Name: <input type="text" name="city_name" placeholder="city_name"  class="form-control" required="">
+                        </div>
+
+						<div class="top-inputs">
+                        City Name: <input type="text" name="city_name" placeholder="city_name"  class="form-control" required="">
+                        </div>
+
+						<div class="top-inputs">    
+                        Customer Address<input type="text" name="cust_add" placeholder="cust_add" class="form-control" required="">
+                        </div>
+
+						<div class="top-inputs">    
+                         Contact No:<input type="text" name="contact_no" placeholder="contact_no" class="form-control" required="">
+                        </div>
+
+						
+						<div class="top-inputs">    
+                         Email:<input type="text" name="email" placeholder="email" class="form-control" required="">
+                        </div>
+
+						
+						<div class="top-inputs">    
+                         User Name:<input type="text" name="username" placeholder="username" class="form-control" required="">
+                        </div>
+
+						
+					
+                        <!--<div class="top-inputs">    
+                         Destination:<input type="text" name="destination" placeholder="Destination" class="form-control" required="">
+                          <select name="destination" class="form-control">
+                            <option value="Ahmedabad">Ahmedabad</option>
+                            <option value="Indore">Indore</option>
+                            <option value="Surat">Surat</option>
+                            <option value="Bhopal">Bhopal</option>
+                            <option value="Rajkot">Rajkot</option>
+                        </select>
+                       </div>
+                        <div class="top-inputs">    
+                         Date:<input type="date" name="book_date" placeholder="Date" class="form-control" required="">
+                        </div>
+                        <div class="top-inputs">    
+                         Payment Type:
+                         <input type="radio" name="payment_type" value="Cash" class="form-control" required="">Cash
+                         <input type="radio" name="payment_type" value="Card" class="form-control" required="">Card
+                       </div>-->
+
+                        <div class="text-right">
+                            <button type="submit" name="submit" class="btn btn-theme2">Book Now</button>
+                        </div>
+                    </form>
+                </div>
+                
+                </div>
             </div>
-			
-			
-			 <div class="clearfix"> </div>
-
-			 <div class="col-md-12 form-group1">
-              <label class="control-label">City Name</label>
-              <input type="text" placeholder="city_name" name="city_name" value="<?php echo $fetch->city_name;?>" >
-            </div>
-
-			<div class="col-md-12 form-group1">
-              <label class="control-label">Customer Address</label>
-              <input type="text" placeholder="cust_add" name="cust_add" value="<?php echo $fetch->cust_add;?>" >
-            </div>
-
-			<div class="col-md-12 form-group1">
-              <label class="control-label">Contact No</label>
-              <input type="text" placeholder="contact_no" name="contact_no" value="<?php echo $fetch->contact_no;?>" >
-            </div>
-
-			<div class="col-md-12 form-group1">
-              <label class="control-label">Email</label>
-              <input type="text" placeholder="email" name="email" value="<?php echo $fetch->email;?>" >
-            </div>
-
-			<div class="col-md-12 form-group1">
-              <label class="control-label">User Name</label>
-              <input type="text" placeholder="username" name="username" value="<?php echo $fetch->username;?>" >
-            </div>
+        </div>
+     
+    </div>
+</section>
 
 
-
-			 <div class="clearfix"> </div>
-            
-			
-	
-             <div class="clearfix"> </div>
-   
-             <div class="clearfix"> </div>
-          
-            <div class="col-md-12 form-group">
-              <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-              <button type="reset" class="btn btn-default">Reset</button>
-            </div>
-          <div class="clearfix"> </div>
-        </form>
-    
- 	<!---->
 <?php
 include_once('footer.php');
 ?>
